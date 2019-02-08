4d3e932a0040df5e2c82eba9f247accc2a21e153
sparksparker <sehyunspark@gmail.com>
2019-02-08 10:28:24 -0800

    Set up route

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+// Require the controllers
+const controller = require('../controllers/index.js');
+
+// Test url to check all of the files 
+// are communicating correctly
+router.get('/', (req, res) => {
+  res.redirect('/restaurants');
+});
+
+
+
+module.exports = router;
