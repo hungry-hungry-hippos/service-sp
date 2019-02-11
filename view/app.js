a079b753129a5571164cb76e45b5a5a5d08f59ea
sparksparker <sehyunspark@gmail.com>
2019-02-11 14:08:12 -0800

    Update GET request

--- view/app.js
+++ view/app.js
@@ -1,36 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const indexRouter = require('../routes/index.js');
-const restaurantsRouter = require('../routes/restaurants.js');
 const path = require('path');
-const mongoose = require('mongoose');
-// const mongoDB = 'mongodb://sparksparker:test123@ds225205.mlab.com:25205/restaurants-test';
-// mongoose.connect(mongoDB, { useNewUrlParser: true} );
-// mongoose.Promise = global.Promise;
 const { defaultRestaurants } = require('../models/index.js');
-// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-const port = 3010;
-
 const app = express();
-let pathToAssets = path.join(__dirname, '../client/dist');
-
+const pathToAssets = path.join(__dirname, '../client/dist');
 const staticAssets = express.static(pathToAssets);
+const port = process.env.PORT || 3010;
 
+// app.use(staticAssets);
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+// app.use(bodyParser.urlencoded({extended: false}));
 
-// app.use('/', indexRouter);
-// app.use('/restaurants', restaurantsRouter);
+app.get('/', (req, res) => {
+  res.send('app.get working');
+});
 
-app.get('/restaurants', (req, res) => {
-  defaultRestaurants((err, data) => {
-    if (err) {
-      res.sendStatus(400);
-      return;
-    }
-    res.status(200).send(data);
-  })
+app.get('/api/restaurants/:id', (req, res) => {
+  let id = req.params.id;
+  defaultRestaurants(id, (err, data) => {
+    return err ?
+    res.sendStatus(400) :
+    res.send(data);
+  });
 });
 
 app.listen(port, () => 
