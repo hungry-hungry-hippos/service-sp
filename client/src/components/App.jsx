a77b22889e7451c961691d52d06038ea10c5c274
sparksparker <sehyunspark@gmail.com>
2019-02-11 17:08:43 -0800

    Change API URL to correct route

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,13 +1,50 @@
 import React, { Component } from 'react';
 
+const API_URL = 'http://localhost:3010/api/restaurants/1';
+
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isLoaded: false,
+      data: [],
+    }
   }
+
+  componentDidMount() {
+    fetch(API_URL)
+    .then(res => res.json())
+    .then(json => {
+      this.setState({
+        isLoaded: true,
+        data: json,
+      });
+    });
+  }
+
   render() {
+
+    let { isLoaded, data } = this.state;
+
+    if (!isLoaded) {
+      return (
+        <div>
+          Loading...
+        </div>
+      )
+    }
     return (
       <div
         className='restaurants'>
+        <ul>
+        data loaded
+          {data.map(restaurant => (
+            <p
+              key={restaurant.id}>
+              {restaurant.name}
+            </p>
+          ))}
+        </ul>
       </div>
     );
   }
