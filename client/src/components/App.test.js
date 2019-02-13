5a5f9b153818ea7eebcb35615bd880526853e28e
sparksparker <sehyunspark@gmail.com>
2019-02-12 17:58:32 -0800

    Rename jest.config file && add in another test

--- client/src/components/App.test.js
+++ client/src/components/App.test.js
@@ -6,11 +6,17 @@ import App from './App.jsx';
 
 import '../../../tests/setup.js';
 import RestaurantList from './RestaurantList';
+import { wrap } from 'module';
 
 describe('App component', () => {
   it('starts with a loadState of false', () => {
     const wrapper = shallow(<App />);
     const loadState = wrapper.state().isLoaded;
     expect(loadState).toEqual(false);
+  });
+  it('starts with an empty data array', () => {
+    const wrapper = shallow(<App />);
+    const dataState = wrapper.state().data;
+    expect(dataState).toEqual([]);
   });
 });
